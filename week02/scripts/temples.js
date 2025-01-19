4456597db99150b32678311aa36ad87eb6773298
BYU-JonGomez <jgomez@byupathway.edu>
2025-01-19 02:24:33 -0300

    2

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -1,36 +1,10 @@
-const date = new Date(document.lastModified);
-
-const currentYear = new Date().getFullYear();
-document.getElementById("current-year").textContent = currentYear;
-
-
-
-
-
-
-
-@media screen and (min-width: 500px) {
-    header {
-        background-color: black;
-        color: white;
-        
-    }
-    main {
-        grid-template-columns: 1fr 1fr;
-    }
-}
-
 $(document).ready(main);
-
 var contador = 1;
-
 function main(){
 	$('.menu_bar').click(function(){
-		// $('nav').toggle(); 
-
 		if(contador == 1){
-			$('nav').animate({
-				left: '0'
+			$('nav').({
+                left: '0'
 			});
 			contador = 0;
 		} else {
@@ -43,3 +17,13 @@ function main(){
 	});
 
 };
+
+const date = new Date(document.lastModified);
+
+const currentYear = new Date().getFullYear();
+document.getElementById("current-year").textContent = currentYear;
+
+
+
+
+
