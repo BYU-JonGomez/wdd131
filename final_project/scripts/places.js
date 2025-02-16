a3162d23b024ea0d7f4826297a1e87f1e5550a34
BYU-JonGomez <jgomez@byupathway.edu>
2025-02-16 04:46:58 -0300

    v5

--- final_project/scripts/places.js
+++ final_project/scripts/places.js
@@ -15,3 +15,27 @@ const date = new Date(document.lastModified);
 
 const currentYear = new Date().getFullYear();
 document.getElementById("current-year").textContent = currentYear;
+
+//localstorage//
+const localstorage = document.querySelector("#nav");
+
+
+function saveName() {
+	let name = document.getElementById("name").value;
+	if (name) {
+		localStorage.setItem("userName", name);
+		alert("You're subscribed. Welcome to our community");
+	} else {
+		alert("Please enter a name.");
+	}
+}
+
+// Function to display the stored name
+function showName() {
+	let savedName = localStorage.getItem("userName");
+	if (savedName) {
+		document.getElementById("result").innerText = "Stored name: " + savedName;
+	} else {
+		document.getElementById("result").innerText = "No name saved.";
+	}
+}
