e9b038a54cedd220cf2d2363ca2bd3c0218459c4
BYU-JonGomez <jgomez@byupathway.edu>
2025-02-16 00:29:09 -0300

    v1

---
+++ final_project/scripts/places.js
@@ -0,0 +1,24 @@
+$(document).ready(main);
+var contador = 1;
+function main(){
+	$('.bt-menu').click(function(){
+		if(contador == 1){
+			$('nav').animate({
+                left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+
+	});
+
+};
+
+const date = new Date(document.lastModified);
+
+const currentYear = new Date().getFullYear();
+document.getElementById("current-year").textContent = currentYear;
